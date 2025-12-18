7125188a768f0cb27a9ab9f83662d1fd013878fa
bilenray <raybimlendra@gmail.com>
2025-12-18 00:24:48 -0600

    sensor_interfacing_done

---
+++ phase_1/main.py
@@ -0,0 +1,24 @@
+#region
+from pal.products.qcar import QCar, IS_PHYSICAL_QCAR
+import sensor_interfacing
+#endregion
+
+
+
+#region : Main Execution
+
+# for virtual QCar setup
+if not IS_PHYSICAL_QCAR:
+    import main_setup as qlabs_setup
+    qlabs_setup.setup()
+
+
+def sensor_main():
+    try:
+        sensor_data = sensor_interfacing.sensorInterfacing(taskRate=120, specifiedSamples=600)
+        sensor_data.start(mode="sensor_stats") # read or sensor_stats
+    finally:
+        print("Execution completed.")
+
+if __name__ == "__main__":
+    sensor_main()
